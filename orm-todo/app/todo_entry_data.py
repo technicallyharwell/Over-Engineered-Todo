9465214f20ac1190185bcd0af50f3c5382e27482
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-08-13 15:48:01 -0500

    initial data

---
+++ orm-todo/app/todo_entry_data.py
@@ -0,0 +1,12 @@
+TODO_ENTRIES = [
+    {
+        "id": 1,
+        "key": "test1",
+        "is_complete": False,
+    },
+    {
+        "id": 2,
+        "key": "test2",
+        "is_complete": False,
+    }
+]
