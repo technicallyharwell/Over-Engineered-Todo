dfbed8c7c7543de6db187a187295bfe2800e630b
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-08-13 15:45:40 -0500

    config db with initial todo entries

---
+++ orm-todo/app/db/init_db.py
@@ -0,0 +1,25 @@
+import logging
+from sqlalchemy.orm import Session
+
+from app import crud, schemas
+from app.db import base  # noqa: F401
+from app.todo_entry_data import TODO_ENTRIES
+
+logger = logging.getLogger(__name__)
+
+
+def init_db(db: Session) -> None:
+    """
+    Initialize the database with the initial data.
+    NOTE: tables should already be created with alembic migrations
+    :param db:
+    :return:
+    """
+    for entry in TODO_ENTRIES:
+        entry_in = schemas.TodoEntryCreate(
+            id=entry["id"],
+            key=entry["key"],
+            is_complete=entry["is_complete"]
+        )
+        crud.todo_entry.create(db, obj_in=entry_in)
+        logger.info(f"Created todo entry with key {entry['key']}")
