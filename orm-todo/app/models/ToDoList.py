790024bd3b1e7b62334634f85c8b42169c55f785
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-08-20 00:11:43 -0500

    update README with orm-todo ref

---
+++ orm-todo/app/models/ToDoList.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+# class ToDoList(Base):
+#     """
+#     Model for representing a to-do list with 0 or many entries
+#
+#     Relations:
+#         - Has a one-to-many relationship with ToDoEntry
+#     """
+#     id = Column(Integer, primary_key=True, index=True)
+#     entries = relationship(
+#         "ToDoEntry",
+#         cascade="all, delete-orphan",
+#         back_populates="todolist",
+#         uselist=True,)
