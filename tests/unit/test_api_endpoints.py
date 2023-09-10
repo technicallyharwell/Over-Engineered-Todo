8394f54b07cd54378d31c000da231d913ef7544e
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-09-10 12:35:34 -0500

    Revert forced test failure, CI has been validated

--- tests/unit/test_api_endpoints.py
+++ tests/unit/test_api_endpoints.py
@@ -1,10 +1,6 @@
 from app.crud.base import CRUDBase
 
 
-def test_ci_failure():
-    assert 1 == 2
-
-
 def test_root_endpoint(client):
     response = client.get("/")
     assert response.status_code == 200
