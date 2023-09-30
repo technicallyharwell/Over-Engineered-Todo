8ae9dbc971797ddcb1859db9730862d33ecf8d71
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-09-29 22:00:58 -0500

    FAS-14 : SQLAlchemy2 uplift
    replaced @as_declarative calls with DeclarativeBase class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,15 +1,5 @@
-import typing as t
-
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
-
-class_registry: t.Dict = {}
+from sqlalchemy.orm import DeclarativeBase
 
 
-@as_declarative(class_registry=class_registry)
-class Base:
-    id: t.Any
-    __name__: str
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+class Base(DeclarativeBase):
+    pass
