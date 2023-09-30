117872729e701986686279f07576e2cd9479dba2
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-09-29 22:06:16 -0500

    FAS-14 : SQLAlchemy2 uplift
    remove SQLA1 env var bypass

--- CI/pretest.sh
+++ CI/pretest.sh
@@ -1,13 +1,9 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 cp ./alembic.ini ./tests/config/alembic.ini
 cp -r ./alembic ./tests/config/alembic
 
 cd ./tests/config && poetry run alembic upgrade head
-
-# @as_declarative decorator was deprecated with SQLAlchemy 2.0 release
-# TODO - refactor SQLA usage to be compatible with 2.0
-export SQLALCHEMY_SILENCE_UBER_WARNING=1
 
 cd ../../
 poetry run coverage run -m pytest
