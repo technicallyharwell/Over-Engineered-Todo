5538d84969a69d2837d021f6a382ee357fa88b85
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-10-04 11:39:30 -0500

    FAS-42
    refactor to use PK id
    
    add mocks to avoid making any DB requests

--- tests/unit/test_api_endpoints.py
+++ tests/unit/test_api_endpoints.py
@@ -1,4 +1,5 @@
 from app.crud.base import CRUDBase
+from app.crud.crud_entry import CRUDToDoEntry
 
 
 def test_root_endpoint(client):
@@ -16,7 +17,7 @@ def test_get_entry(client, monkeypatch):
     # CRUDBase get method is mocked so that the database is not written to for testing
     monkeypatch.setattr(CRUDBase, "get", mock_get)
 
-    response = client.get("/entry/first")
+    response = client.get("/entry/1")
     assert response.status_code == 200
     assert response.json()["key"] == "first"
 
@@ -28,23 +29,42 @@ def test_get_entry_not_found(client, monkeypatch):
     # CRUDBase get method is mocked so that the database is not written to for testing
     monkeypatch.setattr(CRUDBase, "get", mock_get)
 
-    response = client.get("/entry/first")
+    response = client.get("/entry/1")
     assert response.status_code == 404
 
 
-def test_create_entry(client):
-    test_entry = {"id": 1, "key": "first", "is_complete": False}
+def test_create_entry(client, monkeypatch):
+    test_entry = {"id": 1, "key": "create", "is_complete": False}
+
+    def mock_post(*args, **kwargs):
+        return test_entry
+
+    # CRUDBase create method is mocked so that the database is not written to for testing
+    monkeypatch.setattr(CRUDBase, "create", mock_post)
 
     response = client.post("/entry", json=test_entry)
     assert response.status_code == 201
-    assert response.json()["key"] == "first"
+    assert response.json()["key"] == "create"
 
 
-def test_update_entry(client):
-    test_entry = {"id": 1, "key": "first", "is_complete": False}
-    client.post("/entry", json=test_entry)
+def test_update_entry(client, monkeypatch):
+    test_entry = {"id": 1, "key": "update", "is_complete": False}
+    update_entry = {"id": 1, "key": "update", "is_complete": True}
 
-    update_entry = {"id": 1, "key": "first", "is_complete": True}
+    def mock_create(*args, **kwargs):
+        return test_entry
+
+    def mock_update(*args, **kwargs):
+        updated_entry = kwargs["obj_in"]
+        updated_field = updated_entry.is_complete
+        test_entry["is_complete"] = updated_field
+        return test_entry
+
+    # CRUDBase get method is mocked so that update thinks the entry exists
+    monkeypatch.setattr(CRUDBase, "get", mock_create)
+
+    # CRUDToDoEntry update method is mocked so that the database is not written to for testing
+    monkeypatch.setattr(CRUDToDoEntry, "update", mock_update)
 
     response = client.put("/entry", json=update_entry)
     assert response.status_code == 200
@@ -58,15 +78,27 @@ def test_update_entry_not_found(client):
     assert response.status_code == 404
 
 
-def test_delete_entry(client):
-    test_entry = {"id": 1, "key": "first", "is_complete": False}
-    client.post("/entry", json=test_entry)
+def test_delete_entry(client, monkeypatch):
+    test_entry = {"id": 1, "key": "delete", "is_complete": False}
 
-    response = client.delete("/entry/first")
+    def mock_get(*args, **kwargs):
+        from app.models.ToDoEntry import ToDoEntry
+        return ToDoEntry(**test_entry)
+
+    def mock_delete(*args, **kwargs):
+        return test_entry
+
+    # CRUDBase get method is mocked so that delete thinks the entry exists
+    monkeypatch.setattr(CRUDBase, "get", mock_get)
+
+    # CRUDBase delete method is mocked so that the database is not written to for testing
+    monkeypatch.setattr(CRUDBase, "remove", mock_delete)
+
+    response = client.delete("/entry/1")
     assert response.status_code == 200
-    assert response.json()["key"] == "first"
+    assert response.json()["key"] == "delete"
 
 
 def test_delete_entry_not_found(client):
-    response = client.delete("/entry/non_existent_entry")
+    response = client.delete("/entry/1")
     assert response.status_code == 404
