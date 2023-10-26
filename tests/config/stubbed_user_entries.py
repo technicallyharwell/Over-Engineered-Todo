6360643424f0a3822d19fa7a12ccf1b33415ac99
Robert <20748700+technicallyharwell@users.noreply.github.com>
2023-10-25 22:18:39 -0500

    FAS-13 : add unit tests for CRUDUser

---
+++ tests/config/stubbed_user_entries.py
@@ -0,0 +1,14 @@
+USER_TESTING_ENTRIES = [
+    {
+        "username": "test_user_1",
+        "password": "test_password_1",
+    },
+    {
+        "username": "test_user_2",
+        "password": "test_password_2",
+    },
+    {
+        "username": "test_user_2",
+        "password": "test_password_3",
+    }
+]
